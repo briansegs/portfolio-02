6f0f76ea3556df3d567f2cff290dd9a01150dd9f
bsegers <briansegs@gmail.com>
2024-05-22 20:03:36 -0400

    fix: Linting

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
