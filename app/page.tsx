bbbf8145091e16e0948f6c0d159551375c7a85bb
bsegers <briansegs@gmail.com>
2024-05-22 20:03:13 -0400

    fix: Linting

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+    <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
+      <div className="w-full max-w-7xl">
         <h1 className="text-white">Hello, Portfolio!</h1>
       </div>
     </main>
